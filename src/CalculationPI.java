024702df21ce5a801f32f0a93eabe19ccd0e996c
NeubauerSte <stefan.neubauer@htl-wels.at>
2020-10-13 13:15:12 +0200

    Version 3 - Nur noch Algorithmus fehlt

--- src/CalculationPI.java
+++ src/CalculationPI.java
@@ -7,37 +7,49 @@ Erstes Projekt mit Github und Git Bash
 --> Pi berechnen, ziemlich simpel, jedoch muss alles mit Git funktionieren.
  */
 
+import java.util.Scanner;
 
 public class CalculationPI {
-    public static double PI;
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wie oft soll multipliziert werden für PI?");
+        int basis = sc.nextInt();
 
+        System.out.println(piFormelEuler(basis));
+        System.out.println(piWallProdukt(basis));
+        System.out.println(piLeibnitzReihe(basis));
+        System.out.println(piKelallurFormel(basis));
+
+
+
+    }
+
+    static String piWallProdukt(int basis){
+        String ausgabe="Das Ergebnis mit dem Wallprodukt für Pi mit der Basis "+basis+" lautet:\n";
 
 
 
 
     }
 
-    static double wallProdukt(int basis){
+    static String piLeibnitzReihe(int basis){
+        String ausgabe="Das Ergebnis mit der Leibnitzreihe für Pi mit der Basis "+basis+" lautet:\n";
+
+    }
+
+    static String piKelallurFormel(int basis){
+        String ausgabe="Das Ergebnis mit der Kellalurformel für Pi mit der Basis "+basis+" lautet:\n";
+
+    }
+
+    static String piFormelEuler(int basis){
+        String ausgabe="Das Ergebnis mit der Formel von Euler für Pi mit der Basis "+basis+" lautet:\n";
 
 
     }
 
-    static double leibnitzReihe(int basis){
 
-
-    }
-
-    static double kelallurFormel(int basis){
-
-
-    }
-
-    static double formelEuler(int basis){
-
-
-    }
 
 
 }
