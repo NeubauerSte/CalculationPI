0101ef2e278ab0bcf5c79eb70790da1f86c37cd9
NeubauerSte <stefan.neubauer@htl-wels.at>
2020-10-13 13:01:26 +0200

    Version 2 - Grundgerüst

--- src/CalculationPI.java
+++ src/CalculationPI.java
@@ -3,12 +3,41 @@ Autor: Stefan Neubauer
 Erstellt am 12.10.2020
 
 Beschreibung:
-
+Erstes Projekt mit Github und Git Bash
+--> Pi berechnen, ziemlich simpel, jedoch muss alles mit Git funktionieren.
  */
 
 
 public class CalculationPI {
+    public static double PI;
     public static void main(String[] args) {
 
+
+
+
+
+
     }
+
+    static double wallProdukt(int basis){
+
+
+    }
+
+    static double leibnitzReihe(int basis){
+
+
+    }
+
+    static double kelallurFormel(int basis){
+
+
+    }
+
+    static double formelEuler(int basis){
+
+
+    }
+
+
 }
